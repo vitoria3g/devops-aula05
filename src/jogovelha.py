0fd7ea8c4b5db99417ba4013b9a95213536217ec
Vitoria Carolina <viihackles9@gmail.com>
2020-09-27 22:07:10 -0300

    testes Travis

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -5,7 +5,7 @@ def inicializar():
     for i in range(3):
         linha = []
         for j in range(3):
-            linha.append(".")
+            linha.ap pend("X")
         tab.append(linha)
     return tab
 
